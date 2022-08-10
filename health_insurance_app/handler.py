aee93c85151c8601f60400dcf132d556f6705678
Thiago Borges Lima <thiago.blima@hotmail.com>
2022-08-10 17:58:17 -0300

    CRISP2

--- health_insurance_app/handler.py
+++ health_insurance_app/handler.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from healthinsurance.HealthInsurance import HealthInsurance
 
 # loading model
-model = pickle.load( open( 'models/model_linear_regression.pkl', 'rb' ) )
+model = pickle.load( open( 'models/model_xgb.pkl', 'rb' ) )
 
 # initialize API
 app = Flask( __name__ )
